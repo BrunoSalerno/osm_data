851375a81d0cb360b48bcaf222e66db9df58fd00
Bruno Salerno <br.salerno@gmail.com>
2019-08-21 12:16:40 +0200

    Improve way test

--- test/unit/way_test.rb
+++ test/unit/way_test.rb
@@ -2,6 +2,7 @@ require_relative "../config"
 
 describe OSMData::Way do
   let (:way) {OSMData::Way.new}
+  let (:way2) {OSMData::Way.new}
   let (:node) {OSMData::Node.new(lat: 23, lon: 22)}
 
   it "should allow node members" do
@@ -9,7 +10,9 @@ describe OSMData::Way do
     assert_equal [node], way.members
   end
 
-  #it "shouldn't allow other kind of members" do
-
-  #end
+  it "shouldn't allow other kind of members" do
+    assert_raises OSMData::InvalidMemberError do
+      way << way2
+    end
+  end
 end
