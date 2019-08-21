6a72e0fe61937921a0a91a098265ee670899efd6
Bruno Salerno <br.salerno@gmail.com>
2019-08-21 23:05:18 +0200

    Add refs to XML writer

--- lib/writers/xml.rb
+++ lib/writers/xml.rb
@@ -13,11 +13,13 @@ module OSMData
             end
             ways.map do |way|
               xml.way(id: way.id) {
+                refs_for way, xml
                 tags_for way, xml
               }
             end
             relations.map do |relation|
               xml.relation(id: relation.id) {
+                refs_for relation, xml
                 tags_for relation, xml
               }
             end
@@ -28,6 +30,16 @@ module OSMData
       end
 
       private
+
+      def refs_for(element, xml)
+        element.members.each do |member|
+          if element.is_a?(Way)
+            xml.nd(ref: member.id)
+          elsif element.is_a?(Relation)
+            xml.member(type: member.class.name.downcase.split('::').last, ref: member.id)
+          end
+        end
+      end
 
       def tags_for(element, xml)
         element.tags.each do |k,v|
--- test/unit/writers/xml_test.rb
+++ test/unit/writers/xml_test.rb
@@ -9,18 +9,28 @@ describe OSMData::Writer::XML do
     OSMData::Way.new(id: -109, members: [node], tags: {route: 'bus', type: 'route'})
   end
 
+  let (:rel) do
+    OSMData::Relation.new(id: -23, members: [way], tags: {name: 'Bus 55'})
+  end
+
   let (:col) {OSMData::ElementCollection.new}
 
   it 'should build a proper XML' do
-    col << way
+    col << rel
+
     expected_xml = <<XML
 <?xml version="1.0"?>
 <osm>
   <node id="-12" lat="23" lon="32"/>
   <way id="-109">
+    <nd ref="-12"/>
     <tag k="route" v="bus"/>
     <tag k="type" v="route"/>
   </way>
+  <relation id="-23">
+    <member type="way" ref="-109"/>
+    <tag k="name" v="Bus 55"/>
+  </relation>
 </osm>
 XML
 
